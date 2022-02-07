c037bdbb48e796eb5907f68cf6d98f154bad7e41
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-07 15:24:54 +0300

    Add extension to create database migrations and upgrade database version

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 from app import create_app, db
 from app.models import User, Pitch
 from flask_script import Manager, Server
+from  flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
 
 manager = Manager(app)
-
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
