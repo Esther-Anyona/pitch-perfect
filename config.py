9d26e5cb3018f9118914215751e7158a2e9956ff
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-09 16:22:14 +0300

    Add environment variables to start script

--- config.py
+++ config.py
@@ -3,9 +3,15 @@ import os
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_perfect'
-    SECRET_KEY = b'O\x8aL\x80\xc4s(\xb2<:'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-export SECRET_KEY = 'Flask WTF Secret Key'
+export SECRET_KEY=b'O\x8aL\x80\xc4s(\xb2<:'
+export MAIL_USERNAME=smtpesther@gmail.com
+export MAIL_PASSWORD=smtp12345
+
+python3 manage.py server
