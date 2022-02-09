4be4ebbac150acf2d14bf4662f6d45aa6284b894
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-09 16:32:13 +0300

    Upgrate migration

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_perfect'
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = b'O\x8aL\x80\xc4s(\xb2<:'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     # email configurations
