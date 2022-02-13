24c17a01c911ce2c796a82cc211e09739bd282cd
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-13 23:10:47 +0300

    Change manage.py file

--- manage.py
+++ manage.py
@@ -1,17 +1,17 @@
-from app import create_app, db
-from app.models import User, Pitch
-from flask_script import Manager, Server
-from  flask_migrate import Migrate, MigrateCommand
+from app import create_app
+# from app.models import User, Pitch
+# from flask_script import Manager, Server
+# from  flask_migrate import Migrate, MigrateCommand
 
 app = create_app('production')
 
-manager = Manager(app)
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+# manager = Manager(app)
+# migrate = Migrate(app,db)
+# manager.add_command('db',MigrateCommand)
 
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db,User = User, Pitch=Pitch )
+# @manager.shell
+# def make_shell_context():
+#     return dict(app = app,db = db,User = User, Pitch=Pitch )
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
